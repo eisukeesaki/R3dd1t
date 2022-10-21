f08154140f451a7c5a59ef78002a7fa2cf2addbf
Eisuke Esaki <acekaydev@gmail.com>
2022-10-21 15:39:35 -0700

    created users table seeder script

---
+++ origin/database/seed-table-users.sql
@@ -0,0 +1,10 @@
+INSERT INTO users (id, email, username)
+    VALUES
+        (gen_random_uuid (), 'rei@nerv.com', 'Rei'),
+        (gen_random_uuid (), 'shinji@nerv.com', 'shinji'),
+        (gen_random_uuid (), 'asuka@nerv.com', 'Asuka'),
+        (gen_random_uuid (), 'tohji@nerv.com', 'Tohji'),
+        (gen_random_uuid (), 'kaoru@nerv.com', 'Kaoru'),
+        (gen_random_uuid (), 'mari@nerv.com', 'Mari'),
+        (gen_random_uuid (), 'gendo@nerv.com', 'Gendo');
+
